a81f7afbff04de6d2043470d0db6f7c0602fc713
ImPrimmm <primaxrpla@gmail.com>
2025-11-02 22:22:09 +0700

    Add JavaScript to display numbers and operators on the screen and convert divs into buttons.

---
+++ script.js
@@ -0,0 +1,115 @@
+const screen = document.getElementById('screen');
+const one = document.getElementById('one');
+const two = document.getElementById('two');
+const three = document.getElementById('three');
+const four = document.getElementById('four');
+const five = document.getElementById('five');
+const six = document.getElementById('six');
+const seven = document.getElementById('seven');
+const eight = document.getElementById('eight');
+const nine = document.getElementById('nine');
+const zero = document.getElementById('zero');
+const plus = document.getElementById('plus');
+const minus = document.getElementById('minus');
+const times = document.getElementById('times');
+const divide = document.getElementById('divide');
+const equal = document.getElementById('equal');
+const removeOne = document.getElementById('removeOne');
+
+one.addEventListener('click', () => {
+    let n = 1;
+    for (let i = 0; i < n; i++) {
+        screen.append("1");
+    }
+});
+
+two.addEventListener('click', () => {
+    let n = 1;
+    for (let i = 0; i < n; i++) {
+        screen.append("2");
+    }
+});
+
+three.addEventListener('click', () => {
+    let n = 1;
+    for (let i = 0; i < n; i++) {
+        screen.append("3");
+    }
+});
+
+four.addEventListener('click', () => {
+    let n = 1;
+    for (let i = 0; i < n; i++) {
+        screen.append("4");
+    }
+});
+
+five.addEventListener('click', () => {
+    let n = 1;
+    for (let i = 0; i < n; i++) {
+        screen.append("5");
+    }
+});
+
+six.addEventListener('click', () => {
+    let n = 1;
+    for (let i = 0; i < n; i++) {
+        screen.append("6");
+    }
+});
+
+seven.addEventListener('click', () => {
+    let n = 1;
+    for (let i = 0; i < n; i++) {
+        screen.append("7");
+    }
+});
+
+eight.addEventListener('click', () => {
+    let n = 1;
+    for (let i = 0; i < n; i++) {
+        screen.append("8");
+    }
+});
+
+nine.addEventListener('click', () => {
+    let n = 1;
+    for (let i = 0; i < n; i++) {
+        screen.append("9");
+    }
+});
+
+zero.addEventListener('click', () => {
+    let n = 1;
+    for (let i = 0; i < n; i++) {
+        screen.append("0");
+    }
+});
+
+plus.addEventListener('click', () => {
+    let n = 1;
+    for (let i = 0; i < n; i++) {
+        screen.append("+");
+    }
+});
+
+minus.addEventListener('click', () => {
+    let n = 1;
+    for (let i = 0; i < n; i++) {
+        screen.append("-");
+    }
+});
+
+times.addEventListener('click', () => {
+    let n = 1;
+    for (let i = 0; i < n; i++) {
+        screen.append("x");
+    }
+});
+
+divide.addEventListener('click', () => {
+    let n = 1;
+    for (let i = 0; i < n; i++) {
+        screen.append("/");
+    }
+});
