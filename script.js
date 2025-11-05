710e9835d5b3fa3e0f5ea8e249bf43c0c8e758a7
ImPrimmm <primaxrpla@gmail.com>
2025-11-05 20:59:24 +0700

    Done, but it still needs some bug fixes and also needs to be made responsive and more concise.

--- script.js
+++ script.js
@@ -9,7 +9,15 @@ const seven = document.getElementById("seven");
 const eight = document.getElementById("eight");
 const nine = document.getElementById("nine");
 const zero = document.getElementById("zero");
-let wrapper = [];
+const plus = document.getElementById("plus");
+const minus = document.getElementById("minus");
+const times = document.getElementById("times");
+const divide = document.getElementById("divide");
+const equal = document.getElementById("equal");
+const removeOne = document.getElementById("removeOne");
+const wrapper = [];
+const wrapperConvert = [];
+const removeLastString = [];
 const operator = {
   plus: {
     value: document.getElementById("plus").value,
@@ -27,36 +35,27 @@ const operator = {
     value: document.getElementById("divide").value,
     type: "operator",
   },
-  times: {
-    value: document.getElementById("times").value,
-    type: "operator",
-  },
-  divide: {
-    value: document.getElementById("divide").value,
-    type: "operator",
-  },
-  removeOne: {
-    value: document.getElementById("removeOne").value,
-    type: "specialOperator",
-  },
-  equal: {
-    value: document.getElementById("equal").value,
-    type: "specialOperator",
-  },
 };
 
 one.addEventListener("click", () => {
   if (wrapper.length === 0) {
     wrapper.push("1");
-    screen.append("1")
+    screen.append("1");
   } else {
     const last = wrapper[wrapper.length - 1];
     if (typeof last === "string") {
-      wrapper[wrapper.length - 1] = last + "1";
-      screen.append("1")
+      if (last[0] === "0") {
+        wrapper.splice(last, 1);
+        wrapper.push("1");
+        screen.innerHTML = "";
+        screen.append("1");
+      } else {
+        wrapper[wrapper.length - 1] = last + "1";
+        screen.append("1");
+      }
     } else {
       wrapper.push("1");
-      screen.append("1")
+      screen.append("1");
     }
   }
 });
@@ -64,15 +63,22 @@ one.addEventListener("click", () => {
 two.addEventListener("click", () => {
   if (wrapper.length === 0) {
     wrapper.push("2");
-    screen.append("2")
+    screen.append("2");
   } else {
     const last = wrapper[wrapper.length - 1];
     if (typeof last === "string") {
-      wrapper[wrapper.length - 1] = last + "2";
-      screen.append("2")
+      if (last[0] === "0") {
+        wrapper.splice(last, 1);
+        wrapper.push("2");
+        screen.innerHTML = "";
+        screen.append("2");
+      } else {
+        wrapper[wrapper.length - 1] = last + "2";
+        screen.append("2");
+      }
     } else {
       wrapper.push("2");
-      screen.append("2")
+      screen.append("2");
     }
   }
 });
@@ -80,15 +86,22 @@ two.addEventListener("click", () => {
 three.addEventListener("click", () => {
   if (wrapper.length === 0) {
     wrapper.push("3");
-    screen.append("3")
+    screen.append("3");
   } else {
     const last = wrapper[wrapper.length - 1];
     if (typeof last === "string") {
-      wrapper[wrapper.length - 1] = last + "3";
-      screen.append("3")
+      if (last[0] === "0") {
+        wrapper.splice(last, 1);
+        wrapper.push("3");
+        screen.innerHTML = "";
+        screen.append("3");
+      } else {
+        wrapper[wrapper.length - 1] = last + "3";
+        screen.append("3");
+      }
     } else {
       wrapper.push("3");
-      screen.append("3")
+      screen.append("3");
     }
   }
 });
@@ -96,15 +109,22 @@ three.addEventListener("click", () => {
 four.addEventListener("click", () => {
   if (wrapper.length === 0) {
     wrapper.push("4");
-    screen.append("4")
+    screen.append("4");
   } else {
     const last = wrapper[wrapper.length - 1];
     if (typeof last === "string") {
-      wrapper[wrapper.length - 1] = last + "4";
-      screen.append("4")
+      if (last[0] === "0") {
+        wrapper.splice(last, 1);
+        wrapper.push("4");
+        screen.innerHTML = "";
+        screen.append("4");
+      } else {
+        wrapper[wrapper.length - 1] = last + "4";
+        screen.append("4");
+      }
     } else {
       wrapper.push("4");
-      screen.append("4")
+      screen.append("4");
     }
   }
 });
@@ -112,15 +132,22 @@ four.addEventListener("click", () => {
 five.addEventListener("click", () => {
   if (wrapper.length === 0) {
     wrapper.push("5");
-    screen.append("5")
+    screen.append("5");
   } else {
     const last = wrapper[wrapper.length - 1];
     if (typeof last === "string") {
-      wrapper[wrapper.length - 1] = last + "5";
-      screen.append("5")
+      if (last[0] === "0") {
+        wrapper.splice(last, 1);
+        wrapper.push("5");
+        screen.innerHTML = "";
+        screen.append("5");
+      } else {
+        wrapper[wrapper.length - 1] = last + "5";
+        screen.append("5");
+      }
     } else {
       wrapper.push("5");
-      screen.append("5")
+      screen.append("5");
     }
   }
 });
@@ -128,15 +155,22 @@ five.addEventListener("click", () => {
 six.addEventListener("click", () => {
   if (wrapper.length === 0) {
     wrapper.push("6");
-    screen.append("6")
+    screen.append("6");
   } else {
     const last = wrapper[wrapper.length - 1];
     if (typeof last === "string") {
-      wrapper[wrapper.length - 1] = last + "6";
-      screen.append("6")
+      if (last[0] === "0") {
+        wrapper.splice(last, 1);
+        wrapper.push("6");
+        screen.innerHTML = "";
+        screen.append("6");
+      } else {
+        wrapper[wrapper.length - 1] = last + "6";
+        screen.append("6");
+      }
     } else {
       wrapper.push("6");
-      screen.append("6")
+      screen.append("6");
     }
   }
 });
@@ -144,15 +178,22 @@ six.addEventListener("click", () => {
 seven.addEventListener("click", () => {
   if (wrapper.length === 0) {
     wrapper.push("7");
-    screen.append("7")
+    screen.append("7");
   } else {
     const last = wrapper[wrapper.length - 1];
     if (typeof last === "string") {
-      wrapper[wrapper.length - 1] = last + "7";
-      screen.append("7")
+      if (last[0] === "0") {
+        wrapper.splice(last, 1);
+        wrapper.push("7");
+        screen.innerHTML = "";
+        screen.append("7");
+      } else {
+        wrapper[wrapper.length - 1] = last + "7";
+        screen.append("7");
+      }
     } else {
       wrapper.push("7");
-      screen.append("7")
+      screen.append("7");
     }
   }
 });
@@ -160,15 +201,22 @@ seven.addEventListener("click", () => {
 eight.addEventListener("click", () => {
   if (wrapper.length === 0) {
     wrapper.push("8");
-    screen.append("8")
+    screen.append("8");
   } else {
     const last = wrapper[wrapper.length - 1];
     if (typeof last === "string") {
-      wrapper[wrapper.length - 1] = last + "8";
-      screen.append("8")
+      if (last[0] === "0") {
+        wrapper.splice(last, 1);
+        wrapper.push("8");
+        screen.innerHTML = "";
+        screen.append("8");
+      } else {
+        wrapper[wrapper.length - 1] = last + "8";
+        screen.append("8");
+      }
     } else {
       wrapper.push("8");
-      screen.append("8")
+      screen.append("8");
     }
   }
 });
@@ -176,15 +224,22 @@ eight.addEventListener("click", () => {
 nine.addEventListener("click", () => {
   if (wrapper.length === 0) {
     wrapper.push("9");
-    screen.append("9")
+    screen.append("9");
   } else {
     const last = wrapper[wrapper.length - 1];
     if (typeof last === "string") {
-      wrapper[wrapper.length - 1] = last + "9";
-      screen.append("9")
+      if (last[0] === "0") {
+        wrapper.splice(last, 1);
+        wrapper.push("9");
+        screen.innerHTML = "";
+        screen.append("9");
+      } else {
+        wrapper[wrapper.length - 1] = last + "9";
+        screen.append("9");
+      }
     } else {
       wrapper.push("9");
-      screen.append("9")
+      screen.append("9");
     }
   }
 });
@@ -192,15 +247,15 @@ nine.addEventListener("click", () => {
 zero.addEventListener("click", () => {
   if (wrapper.length === 0) {
     wrapper.push("0");
-    screen.append("0")
+    screen.append("0");
   } else {
     const last = wrapper[wrapper.length - 1];
-    if (typeof last === "string") {
+    if (typeof last === "string" && last[0] !== "0") {
       wrapper[wrapper.length - 1] = last + "0";
-      screen.append("0")
-    } else {
+      screen.append("0");
+    } else if (typeof last !== "string" && last[0] !== "0") {
       wrapper.push("0");
-      screen.append("0")
+      screen.append("0");
     }
   }
 });
@@ -239,8 +294,115 @@ divide.addEventListener("click", () => {
   if (wrapper.length !== 0) {
     const last = wrapper[wrapper.length - 1];
     if (typeof last === "string") {
-      wrapper.push(operator.times);
-      screen.append(operator.times.value);
+      wrapper.push(operator.divide);
+      screen.append("÷");
+    }
+  }
+});
+
+equal.addEventListener("click", () => {
+  wrapperConvert.splice(0, wrapperConvert.length);
+
+  for (let i = 0; i < wrapper.length; i++) {
+    if (typeof wrapper[i] === "string") {
+      wrapperConvert.push(parseInt(wrapper[i]));
+    } else {
+      wrapperConvert.push(wrapper[i].value);
+    }
+  }
+
+  for (let n = 0; n < wrapperConvert.length; n++) {
+    if (typeof wrapperConvert[n] === "string") {
+      if (wrapperConvert[n] === "x") {
+        wrapperConvert[n - 1] = wrapperConvert[n - 1] * wrapperConvert[n + 1];
+        wrapperConvert.splice(n, 2);
+        screen.innerHTML = "";
+        screen.append(wrapperConvert[0]);
+        n = n - 1;
+      } else if (wrapperConvert[n] === "/") {
+        wrapperConvert[n - 1] = wrapperConvert[n - 1] / wrapperConvert[n + 1];
+        wrapperConvert.splice(n, 2);
+        screen.innerHTML = "";
+        screen.append(wrapperConvert[0]);
+        n = n - 1;
+      }
+    }
+  }
+
+  for (let j = 0; j < wrapperConvert.length; j++) {
+    if (typeof wrapperConvert[j] === "string") {
+      if (wrapperConvert[j] === "+") {
+        wrapperConvert[j - 1] = wrapperConvert[j - 1] + wrapperConvert[j + 1];
+        wrapperConvert.splice(j, 2);
+        wrapper.splice(0, wrapper.length);
+        wrapper.push(String(wrapperConvert[0]));
+        screen.innerHTML = "";
+        screen.append(wrapperConvert[0]);
+        j = j - 1;
+      } else if (wrapperConvert[j] === "-") {
+        wrapperConvert[j - 1] = wrapperConvert[j - 1] - wrapperConvert[j + 1];
+        wrapperConvert.splice(j, 2);
+        wrapper.splice(0, wrapper.length);
+        wrapper.push(String(wrapperConvert[0]));
+        screen.innerHTML = "";
+        screen.append(wrapperConvert[0]);
+        j = j - 1;
+      } else {
+        wrapper.splice(0, wrapper.length);
+        screen.innerHTML = "";
+        screen.append(wrapperConvert[0]);
+      }
+    }
+  }
+});
+
+removeOne.addEventListener("click", () => {
+  const last = wrapper[wrapper.length - 1];
+
+  if (typeof last === "string") {
+    for (let i = 0; i < last.length; i++) {
+      removeLastString.push(last[i]);
+    }
+
+    if (typeof removeLastString[0] !== "undefined") {
+      removeLastString.pop();
+      wrapper[wrapper.length - 1] = "";
+
+      for (let n = 0; n < removeLastString.length; n++) {
+        wrapper[wrapper.length - 1] =
+          wrapper[wrapper.length - 1] + removeLastString[n];
+      }
+
+      removeLastString.splice(0, removeLastString.length);
+
+      screen.innerHTML = "";
+
+      for (let j = 0; j < wrapper.length; j++) {
+        if (typeof wrapper[j] === "string") {
+          screen.append(wrapper[j]);
+        } else {
+          screen.append(wrapper[j].value);
+        }
+      }
+
+      if (wrapper[wrapper.length - 1] === "") {
+        wrapper.pop()
+      }
+    } else {
+      removeLastString.splice(0, removeLastString.length);
+      wrapper.pop();
+    }
+  } else {
+    wrapper.splice(wrapper.length - 1, 1);
+
+    screen.innerHTML = "";
+
+    for (let i = 0; i < wrapper.length; i++) {
+      if (typeof wrapper[i] === "string") {
+        screen.append(wrapper[i]);
+      } else {
+        screen.append(wrapper[i].value);
+      }
     }
   }
 });
